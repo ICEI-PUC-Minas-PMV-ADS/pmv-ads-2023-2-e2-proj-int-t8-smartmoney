7c9310f5ede76c46e5d1e58a5de378b844a1b63f
mbeaatriz <mbeaatriz@gmail.com>
2023-10-08 20:25:53 -0300

    Adicionando a tabela Usuarios

--- src/smartmoney/smartmoney/Models/AppDbContext.cs
+++ src/smartmoney/smartmoney/Models/AppDbContext.cs
@@ -7,5 +7,7 @@ namespace smartmoney.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
---
+++ src/smartmoney/smartmoney/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace smartmoney.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "obrigatório Informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "obrigatório Informar o email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "obrigatório Informar a senha.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
