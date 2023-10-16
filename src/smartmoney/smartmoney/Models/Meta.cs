e472e1918139c60bf5c500476e47feed7d4adc02
mbeaatriz <mbeaatriz@gmail.com>
2023-10-16 18:54:54 -0300

    Criando tabelas no banco local

---
+++ src/smartmoney/smartmoney/Models/Meta.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace smartmoney.Models
+{
+    [Table("Metas")]
+    public class Meta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o título.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data inicial.")]
+        public DateTime DataInicial { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data final.")]
+        public DateTime DataFinal { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o valor.")]
+        public decimal Valor { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+    }
+}
